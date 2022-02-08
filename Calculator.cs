ba03d9f2de29ea5fcaa4573f311bea95aaae185a
Anandbir Hothi <axh978@cs.bham.ac.uk>
2022-02-08 14:13:37 +0000

    Finished restart loop after user input

--- Calculator.cs
+++ Calculator.cs
@@ -11,87 +11,90 @@ namespace PriceCalculator
 
             do
             {
+                Console.WriteLine("what is the height of the wall?");
+                double heightOfWall = double.Parse(Console.ReadLine());
 
+                Console.WriteLine("What is the width of the wall");
+                double widthOfWall = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Do you want ");
+                double areaOfWall = heightOfWall * widthOfWall;
+
+                Console.WriteLine("Are there areas of the wall that don't need painting? (y/n)");
+                if (Console.ReadLine().Equals("y"))
+                {
+                    Console.WriteLine("what is the height of this area?");
+                    double heightOfArea = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("What is the width of this area?");
+                    double widthOfArea = double.Parse(Console.ReadLine());
+
+                    areaOfWall -= (heightOfArea * widthOfArea);
+                }
+
+                Console.WriteLine("Area of Paintable wall = " + areaOfWall);
+
+                Console.WriteLine("what paint colour are you using?");
+                string paintColour = Console.ReadLine();
+
+                double costOfPaintCans;
+
+                switch (paintColour)
+                {
+                    case "red":
+                        costOfPaintCans = 18.99;
+                        break;
+                    case "blue":
+                        costOfPaintCans = 16.99;
+                        break;
+                    case "green":
+                        costOfPaintCans = 21.99;
+                        break;
+                    default:
+                        costOfPaintCans = 15.99;
+                        break;
+                }
+
+                // Assumption 1 litre paint covers 10m^2 of wall
+                int numOfPaintCans = (int)Math.Ceiling(areaOfWall / 10.0);
+                double totalCostOfPaint = numOfPaintCans * costOfPaintCans;
+
+                Console.WriteLine("the number of 1 litre paint cans you need is " + numOfPaintCans + ", this will Cost £" + Math.Round(totalCostOfPaint, 2));
+
+                Console.WriteLine("How many hours do think it will take to complete the work?");
+                double hoursWorked = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("what is your hourly rate?");
+                double hourlyRate = double.Parse(Console.ReadLine());
+
+                // TODO redo section below 
+                Console.WriteLine("How many miles did you travel?");
+                double milesTraveled = double.Parse(Console.ReadLine());
+                double fuelCostPerMile = 0.12;
+
+                double profitPercent = 0.4;
+                double taxRate = 0.2;
+
+                // TODO Costs calculation section needs to be redone
+                double totalCost = (numOfPaintCans * costOfPaintCans) + (milesTraveled * fuelCostPerMile);
+                //double profit = (totalCost * profitPercent);
+                double costForCustomer = totalCost + (hoursWorked * hourlyRate);
+                double profitAfterTax = (costForCustomer - (costForCustomer * taxRate)) - totalCost;
+
+                Console.WriteLine("Cost of goods and travel = £" + Math.Round(totalCost, 2));
+                //Console.WriteLine("In order to earn " + (profitPercent * 100) + "% profit charge £" + Math.Round(costForCustomer, 2));
+                Console.WriteLine("Profit made after tax = £" + Math.Round(profitAfterTax, 2));
+
+                Console.WriteLine("Do you want to restart the program? (y/n)");
+                if (Console.ReadLine().Equals("y"))
+                {
+                    isFinished = true;
+                }
+                else
+                {
+                    isFinished = false;
+                }
             }
             while (isFinished);
-
-
-            Console.WriteLine("what is the height of the wall?");
-            double heightOfWall = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("What is the width of the wall");
-            double widthOfWall = double.Parse(Console.ReadLine());
-
-            double areaOfWall = heightOfWall * widthOfWall;
-
-            Console.WriteLine("Are there areas of the wall that don't need painting? (y/n)");
-            if (Console.ReadLine().Equals("y"))
-            {
-                Console.WriteLine("what is the height of this area?");
-                double heightOfArea = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("What is the width of this area?");
-                double widthOfArea = double.Parse(Console.ReadLine());
-
-                areaOfWall -= (heightOfArea * widthOfArea);
-            }
-
-            Console.WriteLine("Area of Paintable wall = " + areaOfWall);
-
-            Console.WriteLine("what paint colour are you using?");
-            string paintColour = Console.ReadLine();
-
-            double costOfPaintCans;
-
-            switch (paintColour)
-            {
-                case "red":
-                    costOfPaintCans = 18.99;
-                    break;
-                case "blue":
-                    costOfPaintCans = 16.99;
-                    break;
-                case "green":
-                    costOfPaintCans = 21.99;
-                    break;
-                default:
-                    costOfPaintCans = 15.99;
-                    break;
-            }
-
-            // Assumption 1 litre paint covers 10m^2 of wall
-            int numOfPaintCans = (int) Math.Ceiling(areaOfWall/10.0);
-            double totalCostOfPaint = numOfPaintCans * costOfPaintCans;
-
-            Console.WriteLine("the number of 1 litre paint cans you need is " + numOfPaintCans + ", this will Cost £" + Math.Round(totalCostOfPaint, 2));
-
-            double costOfDecorations = 119.99;
-
-            Console.WriteLine("How many hours do think it will take to complete the work?");
-            double hoursWorked = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("what is your hourly rate?");
-            double hourlyRate = 8.0;
-
-            // ---------- TODO redo section below ------------
-            Console.WriteLine("How many miles did you travel?");
-            double milesTraveled = double.Parse(Console.ReadLine());
-            double fuelCostPerMile = 0.12;
-
-            double profitPercent = 0.4;
-            double taxRate = 0.2;
-
-            // TODO Costs calculation section needs to be redone
-            double totalCost = (numOfPaintCans * costOfPaintCans) + (milesTraveled * fuelCostPerMile) + costOfDecorations;
-            //double profit = (totalCost * profitPercent);
-            double costForCustomer = totalCost + (hoursWorked * hourlyRate);
-            double profitAfterTax = (costForCustomer - (costForCustomer * taxRate)) - totalCost;
-
-            Console.WriteLine("Cost of goods and travel = £" + Math.Round(totalCost, 2));
-            //Console.WriteLine("In order to earn " + (profitPercent * 100) + "% profit charge £" + Math.Round(costForCustomer, 2));
-            Console.WriteLine("Profit made after tax = £" + Math.Round(profitAfterTax, 2));   
         }
     }
 }
