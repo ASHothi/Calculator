fb34851582dd97185e2249c0a520ae40ae068262
Anandbir Hothi <axh978@cs.bham.ac.uk>
2022-02-08 10:41:11 +0000

    Added user input and Todo comments

--- Program.cs
+++ Program.cs
@@ -9,11 +9,14 @@ namespace PriceCalculator
     {
         static void Main(string[] args)
         {
-            int numOfPaintCans = 10;
+            Console.WriteLine("How many paint cans did you Buy?");
+            int numOfPaintCans = int.Parse(Console.ReadLine());
+            
             double costOfPaintCans = 15.99;
             double costOfDecorations = 119.99;
 
-            double daysWorked = 2.5;
+            Console.WriteLine("How many days did you work?");
+            double daysWorked = double.Parse(Console.ReadLine());
             double dayRate = 25.0;
 
             double milesTraveled = 5.29;
@@ -27,9 +30,13 @@ namespace PriceCalculator
             double costForCustomer = totalCost + profit + (daysWorked * dayRate);
             double profitAfterTax = profit - (profit * taxRate);
 
-            Console.WriteLine("Cost of goods and travel is £" + totalCost + ".");
-            Console.WriteLine("In order to earn " + (profitPercent * 100) + "% profit charge £" + Math.Round(costForCustomer, 2) + ".");
-            Console.WriteLine("Profit made after tax = £" + Math.Round(profitAfterTax, 2) + ".");
+            Console.WriteLine("Cost of goods and travel = £" + Math.Round(totalCost, 2));
+            Console.WriteLine("In order to earn " + (profitPercent * 100) + "% profit charge £" + Math.Round(costForCustomer, 2));
+            Console.WriteLine("Profit made after tax = £" + Math.Round(profitAfterTax, 2));
         }
     }
 }
+
+// TODO User Input through readline
+// TODO redo Costs section calculation
+// TODO Add verfication for the ReadLine to make sure only numbers are entered 
